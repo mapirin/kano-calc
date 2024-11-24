545adeaff6cadc65758ead4f6d69e46f019a29b4
'mapirin' <'812matahiro29@gmail.com'>
2024-11-24 17:17:57 +0900

    20241124

--- kano-calculator/src/main/java/com/kano/calc/web/KanoCalcController.java
+++ kano-calculator/src/main/java/com/kano/calc/web/KanoCalcController.java
@@ -1,47 +1,46 @@
 package com.kano.calc.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/web")
+@Controller
 public class KanoCalcController {
 
 	@GetMapping("/title")
 	public String title() {
-		return "/web/calc/title";
+		return "web/calc/title";
 	}
-	
-	@GetMapping("/pl-calc")
+
+	@PostMapping(value="/pl-calc",params="plCalc")
 	public String pagePlCalc() {
-		return "/web/calc/pl-calc";
+		return "web/calc/plCalc";
 	}
-	
-	@GetMapping("/trip-calc")
-	public String pageTripCalc() {
-		return "/web/calc/trip-calc";
-	}
-	
-	@GetMapping("/snowboard-calc")
-	public String pageSnowboardCalc() {
-		return "/web/calc/snowboard/calc";
-	}
-	
-	@PostMapping("/pl-calc-exec")
-	public String execPlCalc() {
-		return "/web/calc/pl-calc";
-	}
-	
-	@PostMapping("/trip-calc-exec")
-	public String execTripClac() {
-		return "/web/calc/trip-calc";
-	}
-	
-	@PostMapping("snowboard-calc-exec")
-	public String execSnowboardCalc() {
-		return "/web/calc/snowboard-calc";
-	}
-	
+
+//	@GetMapping("/trip-calc")
+//	public String pageTripCalc() {
+//		return "/web/calc/trip-calc";
+//	}
+//	
+//	@GetMapping("/snowboard-calc")
+//	public String pageSnowboardCalc() {
+//		return "/web/calc/snowboard/calc";
+//	}
+
+//	@PostMapping("/pl-calc-exec")
+//	public String execPlCalc() {
+//		return "/web/calc/pl-calc";
+//	}
+
+//	@PostMapping("/trip-calc-exec")
+//	public String execTripClac() {
+//		return "/web/calc/trip-calc";
+//	}
+//	
+//	@PostMapping("snowboard-calc-exec")
+//	public String execSnowboardCalc() {
+//		return "/web/calc/snowboard-calc";
+//	}
+
 }
